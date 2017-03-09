8d1cda1b0c5d3402501e97cad00a5670cdb6b392
Garth Henson <garth@guahanweb.com>
2017-03-09 09:44:41 -0800

    Fixing the event handling

--- src/bot.ts
+++ src/bot.ts
@@ -40,6 +40,17 @@ export default class bot_handler {
         this.dialog = this.InitializeLuis(config.luis);
         this.SetBotMiddleware();
         this.SetBotDialog();
+
+        this.bot.on('event', (ev) => {
+            if (ev.name === 'connect_agent') {
+                let agentAddress = ev.address.channelId + '/' + ev.address.conversation.id;
+                let customerAddress = ev.value.customerConversationId;
+                queue.update(customerAddress, agentAddress, ev.address);
+                queue.get(customerAddress).messages.forEach((msg) => {
+                    ev.send(msg);
+                });
+            }
+        });
     }
 
     private InitializeLuis(model){
